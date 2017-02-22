2682be359e4462f25c994de5e75bbd01f0ba2084
mattbag <wised@MacBook-Air-di-bagni.local>
2017-02-22 16:28:37 +0100

    fb + check functions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { ModalController, NavController, ActionSheetController } from 'ionic-angular';
-import { AddItemPage } from '../add-item/add-item';
+import { ModalController, NavController, ActionSheetController, AlertController } from 'ionic-angular';
+// import { AddItemPage } from '../add-item/add-item';
 // import { EditItemPage } from '../edit-item/edit-item';
-import { ItemDetailPage } from '../item-detail/item-detail';
+// import { ItemDetailPage } from '../item-detail/item-detail';
 import { AboutPage } from '../about/about';
 // import { Data } from '../../providers/data';
 import { Observable } from 'rxjs/Rx';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'page-home',
@@ -13,29 +14,30 @@ import { Observable } from 'rxjs/Rx';
 })
 export class HomePage {
 
-  public items = [];
-  public addModal = this.modalCtrl.create(AddItemPage);
+  // public items = [];
+  // public addModal = this.modalCtrl.create(AddItemPage);
   public timer: any;
   public running: boolean = false;
-
+  public coffees: FirebaseListObservable<any>;
+  public users: FirebaseListObservable<any>;
+  
   constructor(
     public navCtrl: NavController,
     public modalCtrl: ModalController,
     // public dataService: Data,
     public actionSheetCtrl: ActionSheetController,
+    public af: AngularFire,
+    public alertCtrl: AlertController
   ) {
-
-    // Observable.interval(1000)
-    //         .map((x) => x+1)
-    //         .subscribe((x) => {
-    //           this.timer = (60 * 5 - x) *1000;
-    //         })
-
-
+      this.coffees = af.database.list('/coffees');
+      this.users = af.database.list('/users');
+      // console.log(this.coffees);
+      
   }
   ionViewDidLoad() {
     // if user has no coffee and there is time, pop open this
     // this.addModal.present();
+    // this.addCoffee();
   }
   startTimer() {
     Observable.interval(1000)
@@ -53,129 +55,76 @@ export class HomePage {
     this.running = true;
     this.startTimer();
   }
-  addItem() {
-
-    this.addModal.onDidDismiss((item) => {
-
-      // if (item) {
-      //   this.saveItem(item);
-      // }
-
-    });
-
-    this.addModal.present();
-
+  stopRun() {
+    console.log('Coffee run deleted');
+    // this.running = true;
+    // this.startTimer();
   }
 
-
-  // editItem(item,index) {
-
-  //   let editModal = this.modalCtrl.create(EditItemPage, {item: item, index: index});
-
-  //   editModal.onDidDismiss((item) => {
-  //       console.log(item);
-  //     if (item) {
-  //       console.log('quak');
-
-  //       this.updateItem(item);
-  //     }
-
-  //   });
-
-  //   editModal.present();
-
-  // }
-  uncheck(item) {
-
-  }
-  saveItem(item) {
-
-  }
-  updateItem(item) {
-    console.log(item);
-
-
-  }
-  removeItem(item) {
-
-  }
-
-  viewItem(item) {
-    this.navCtrl.push(ItemDetailPage, {
-      item: item
-    });
-  }
-  // logDrag(event) {
-  //   console.log(event);
-  // }
-  ondrag(event, item, index) {
-    let percent = event.getSlidingPercent();
-    // let index = this.items.indexOf(item);
-    // console.log(event +" "+ item +" "+ index);
-    // console.log(event);
-    // console.log(item);
-    // console.log("index " + index);
-    if (percent > 0) {
-      // positive
-      // console.log('right side');
-
-      // console.log(percent);
-      if (percent > 1) {
-        // this.removeItem(item);
-        setTimeout(() => {
-          event.close();
-        }, 1500)
+addCoffee(){
+let prompt = this.alertCtrl.create({
+    title: 'Howdy!',
+    message: "So which coffee would you like?",
+    inputs: [
+      {
+        name: 'coffee',
+        placeholder: 'Hit me!'
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.users.push({
+            name: 'user', 
+            // user: data.user,
+            coffee: data.coffee,
+            isDone: false
+          });
+        }
       }
-      // if (percent > 1 && !this.oneDeleted) {
-      //   this.oneDeleted = true;
-      //   console.log(index);
-      //   // let cacheIndex = index;
-      //   // let index2 = this.items.indexOf(item);
-      //   //  console.log(index2);
+    ]
+  });
+  prompt.present();
+}
 
-      //   // setTimeout(() => { 
-      //     // event.close();
-      //     // this.items.splice(index, 1);
-      //     this.dataService.save(this.items);
-      //     console.log('deleted ' + index);
-      //   this.oneDeleted = false;
-      //   // }, 300)
-
-      // } else {
-      //   event.close();
-      // }
-      //end if
-    } else {
-      // negative
-      // console.log('left side');
-      // console.log(item);
-      if (percent < -1) {
-        // console.log(this.items[index]);
-        // if (!this.items[index].isDone) {
-        //   // this.items[index].isDone = true;
-        //   // this.dataService.save(this.items);
-        // }
-
-        setTimeout(() => {
-          event.close();
-        }, 500)
-
-        //  console.log(this.items);
-      }
-
-
-    }
-    // if (Math.abs(percent) > 1) {
-    //  let index = this.items.indexOf(item);
-    //   //  console.log(index);
-    //   this.items.splice(index,1);
-    //   this.dataService.save(this.items);
-    // }
+  toggleCheck(user) {
+    // console.log(user.$key);
+    // console.log(key);
+    // user.isDone = !user.isDone;
+    // let key = user.$key;
+    // console.log(key);
+    
+    // let itemObservable = this.af.database.object('/users');
+    // console.log(itemObservable);
+    
+    this.users.update(user.$key,{ isDone: !user.isDone });
   }
-  // shareList(){
-  //   console.log('hello');
+  // saveItem(item) {
 
   // }
+  update(user) {
+    console.log(user);
+    
+    // this.users[].update({ isDone: isDone });
+  }
+  // removeItem(item) {
+
+  // }
+
+  // viewItem(item) {
+  // //   this.navCtrl.push(ItemDetailPage, {
+  // //     item: item
+  // //   });
+  // }
+  
+  
   presentActionSheet() {
     let actionSheet = this.actionSheetCtrl.create({
       // title: 'More Actions',
